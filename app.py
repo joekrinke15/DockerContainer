ef97dc13e1729bf8e09f7a603e4a68fec1f2c22b
EC2 Default User <ec2-user@ip-172-31-77-147.ec2.internal>
2021-02-08 18:04:05 +0000

    Add new app functionality

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 import click
+import math
 
 @click.command()
-@click.option("--name")
-def hello(name):
-    click.echo(f'Hello {name}! I hope you enjoy this app!')
+@click.option('--amount','--term', '--interest')
+def calculate(amount, term, interest):
+    """
+    Calculate the montly payment, interest, and total amount paid on a loan.
+    """
+    nPayments= term * 12
+    rInterest = interest / 12
+    interestPow = math.pow(1 + rInterest)
+    discountFactor = (rInterest - 1)/(interestPow*rInterest)
+    monthlyPayment = amount/discountFactor
+    totalPaid = monthlyPayment * nPayments
+    
+    click.echo(f'Your estimated monthly payment is: {monthlyPayment}. You will pay {totalPaid-amount} in interest and {totalPaid} total.')
 
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
-    hello()
+    calculate()
